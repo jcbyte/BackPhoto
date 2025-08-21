8727a64c37a5e27b6b0a6028c0a8a795b5acd0d5
Joel Cutler <joelcutler108@gmail.com>
2025-08-21 21:31:22 +0100

    add directory navigation

--- adb.py
+++ adb.py
@@ -1,3 +1,5 @@
+from pathlib import PurePosixPath
+
 from ppadb.client import Client as AdbClient
 from ppadb.device import Device as AdbDevice
 
@@ -11,6 +13,17 @@ class Device:
         model = (self.device.shell("getprop ro.product.model") or "unknown").strip()
 
         return f"{manufacturer} {model}"
+
+    def ls(self, path: PurePosixPath) -> list[PurePosixPath]:
+        dir_listing = (self.device.shell(f'ls "{path.as_posix()}" -1') or "").splitlines()
+        return [path / item for item in dir_listing]
+
+    def is_dir(self, path: PurePosixPath) -> bool:
+        result = (d.device.shell(f'stat -c %F "{path.as_posix()}"') or "").strip()
+        return result == "directory"
+
+    def __str__(self) -> str:
+        return f"{self.device.serial} ({self.friendly_name()})"
 
 
 class ADB:
