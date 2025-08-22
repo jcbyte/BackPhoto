9a68990fa5bc51e3fecc425e7613f8eae2705792
Joel Cutler <joelcutler108@gmail.com>
2025-08-22 21:42:31 +0100

    update photo tools to use path

--- photo_tools.py
+++ photo_tools.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from pathlib import Path
 from typing import Any, Callable, Dict, Optional
 
 import piexif
@@ -8,31 +9,32 @@ from PIL import Image
 IMAGE_FORMAT = [".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp", ".heif", ".heic", ".svg", ".ico"]
 
 
-def get_os_time(path: str) -> datetime:
+def get_os_time(path: Path) -> datetime:
     """Returns the last modified time of a file as a datetime object.
 
     Args:
-        path (str): The file path.
+        path (Path): The file path.
 
     Returns:
         datetime: Time of the files last modification.
     """
-    return datetime.fromtimestamp(os.path.getmtime(path))
+    return datetime.fromtimestamp(path.stat().st_mtime)
 
 
-def convert_to_jpg(path: str) -> str:
+def convert_to_jpg(path: Path) -> Path:
     """Converts an image to JPG format, and removes the original file
 
     Args:
-        path (str): The file path of the image to convert.
+        path (Path): The file path of the image to convert.
 
     Returns:
-        str: The new file path of the converted JPG image.
+        Path: The new file path of the converted JPG image.
 
     Raises:
         ValueError: If the image contains transparency.
     """
-    name, ext = os.path.splitext(path)
+    # todo replace path.splitext with path.stem and path.suffix
+    name, ext = os.path.splitext(path.name)
 
     # Check that the file is not already a JPG
     if ext.lower() in [".jpg", ".jpeg"]:
@@ -41,26 +43,28 @@ def convert_to_jpg(path: str) -> str:
     img = Image.open(path)
 
     # If the image has transparency then raise error
-    min_alpha = img.convert("RGBA").getextrema()[-1][0]
+    alpha_channel = img.convert("RGBA").getchannel("A")
+    min_alpha, max_alpha = alpha_channel.getextrema()
+    assert isinstance(min_alpha, int)  # For type checker
     if min_alpha < 255:
         raise ValueError("Image contains transparency")
 
     # Convert and save image as JGP
     img_rgb = img.convert("RGB")
-    new_path = f"{name}.jpg"
+    new_path = path.parent / f"{name}.jpg"
     img_rgb.save(new_path)
 
     # Remove the original file
-    os.remove(path)
+    path.unlink()
 
     return new_path
 
 
-def load_exif(path: str) -> Optional[Dict[str, Any]]:
+def load_exif(path: Path) -> Optional[Dict[str, Any]]:
     """Loads the EXIF data from an image file.
 
     Args:
-        path (str): The file path of the image.
+        path (Path): The file path of the image.
 
     Returns:
         Optional[Dict[str, Any]]: The EXIF data as a dictionary, or None if it cannot be loaded.
@@ -71,35 +75,42 @@ def load_exif(path: str) -> Optional[Dict[str, Any]]:
         return None
 
 
-def save_exif(exif: Dict[str, Any], path: str) -> None:
+def save_exif(exif: Dict[str, Any], path: Path) -> None:
     """Saves EXIF data to an image file.
 
     Args:
         exif (Dict[str, Any]): The EXIF data to save.
-        path (str): The file path of the image.
+        path (Path): The file path of the image.
     """
     exif_bytes = piexif.dump(exif)
     piexif.insert(exif_bytes, path)
 
 
-def validate_exif_time(exif: Dict[str, Any]) -> bool:
-    """Validates if the EXIF data contains necessary time fields.
+def get_exif_time(exif: Dict[str, Any]) -> tuple[None | datetime, bool]:
+    """Gets and validates if the EXIF data contains necessary time fields.
 
     Args:
-        exif (Dict[str, Any]): The EXIF data to validate.
+        exif (Dict[str, Any]): The EXIF data to use.
 
     Returns:
-        bool: True if all required time fields are present, False otherwise.
+        tuple[None | datetime, bool]:
+        - The date in the exif data.
+        - True if all required time fields are present, False otherwise.
     """
-    # Check all creation time based fields
-    if piexif.ImageIFD.DateTime not in exif["0th"]:
-        return False
-    if piexif.ExifIFD.DateTimeDigitized not in exif["Exif"]:
-        return False
-    if piexif.ExifIFD.DateTimeOriginal not in exif["Exif"]:
-        return False
+    # Get all creation time based fields
+    dt0_time: bytes | None = exif.get("0th", {}).get(piexif.ImageIFD.DateTime)
+    dt_digitized_time: bytes | None = exif.get("Exif", {}).get(piexif.ExifIFD.DateTimeDigitized)
+    dt_original_time: bytes | None = exif.get("Exif", {}).get(piexif.ExifIFD.DateTimeOriginal)
 
-    return True
+    time = None
+    time_bytes = dt0_time or dt_digitized_time or dt_original_time
+    if time_bytes:
+        time_str = time_bytes.decode("utf-8")
+        time = datetime.strptime(time_str, "%Y:%m:%d %H:%M:%S")
+
+    missing_fields = dt0_time is None or dt_digitized_time is None or dt_original_time is None
+
+    return time, missing_fields
 
 
 def set_exif_time(exif: Dict[str, Any], time: datetime) -> None:
@@ -109,17 +120,17 @@ def set_exif_time(exif: Dict[str, Any], time: datetime) -> None:
         exif (Dict[str, Any]): The EXIF data to update.
         time (datetime): The datetime value to set.
     """
-    time = time.strftime("%Y:%m:%d %H:%M:%S")
-    exif["0th"][piexif.ImageIFD.DateTime] = time
-    exif["Exif"][piexif.ExifIFD.DateTimeDigitized] = time
-    exif["Exif"][piexif.ExifIFD.DateTimeOriginal] = time
+    time_str = time.strftime("%Y:%m:%d %H:%M:%S")
+    exif["0th"][piexif.ImageIFD.DateTime] = time_str
+    exif["Exif"][piexif.ExifIFD.DateTimeDigitized] = time_str
+    exif["Exif"][piexif.ExifIFD.DateTimeOriginal] = time_str
 
 
-def set_photo_exif_time(file_path: str, log: Optional[Callable[[str], None]] = print) -> None:
+def set_photo_exif_time(file_path: Path, log: Optional[Callable[[str], None]] = print) -> None:
     """Sets the EXIF time of an image based on its file modification time.
 
     Args:
-        file_path (str): The file path of the image.
+        file_path (Path): The file path of the image.
         log (Optional[Callable[[str], None]], optional): Logging function to display messages. Defaults to print.
     """
     ext = os.path.splitext(file_path)[1].lower()
@@ -131,12 +142,13 @@ def set_photo_exif_time(file_path: str, log: Optional[Callable[[str], None]] = p
     try:
         exif = load_exif(file_path)
         if exif:
-            # DOnt update if EXIF time already exists
-            if validate_exif_time(exif):
+            # Don't update if EXIF time already exists
+            exif_time, missing_fields = get_exif_time(exif)
+            if not missing_fields:
                 return
 
             # If EXIF exists but no time, then just add this and save it back
-            set_exif_time(exif, get_os_time(file_path))
+            set_exif_time(exif, exif_time or get_os_time(file_path))
             save_exif(exif, file_path)
         else:
             # If EXIF data does not exist create a minimal one containing time data
@@ -146,20 +158,22 @@ def set_photo_exif_time(file_path: str, log: Optional[Callable[[str], None]] = p
             file_path = convert_to_jpg(file_path)
             save_exif(exif, file_path)
 
-        log(f'Updated: "{os.path.basename(file_path)}"')
+        if log:
+            log(f'Updated: "{os.path.basename(file_path)}"')
 
     except:
-        log(f'Error: "{os.path.basename(file_path)}"')
+        if log:
+            log(f'Error: "{os.path.basename(file_path)}"')
 
 
-def set_photos_exif_time(folder_path: str, log: Optional[Callable[[str], None]] = print) -> None:
+def set_photos_exif_time(folder_path: Path, log: Optional[Callable[[str], None]] = print) -> None:
     """Sets or updates the EXIF time for all images in a folder.
 
     Args:
-        folder_path (str): The path to the folder containing images.
+        folder_path (Path): The path to the folder containing images.
         log (Optional[Callable[[str], None]], optional): Logging function to display messages. Defaults to print.
     """
-    for root, dirs, files in os.walk(folder_path):
-        for filename in files:
-            file_path = os.path.join(root, filename)
+    for file_path in folder_path.rglob("*"):
+        # skip directories
+        if file_path.is_file():
             set_photo_exif_time(file_path, log)
