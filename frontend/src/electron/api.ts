01a5cab068fec9e173380d14290e7ee8438c868b
Joel Cutler <joelcutler108@gmail.com>
2025-08-25 12:54:56 +0100

    now use electron forge with webpack

--- frontend/src/electron/api.ts
+++
@@ -1,8 +0,0 @@
-import { ipcMain, type IpcMainInvokeEvent } from "electron";
-
-ipcMain.handle("node-version", (event: IpcMainInvokeEvent, msg: string): string => {
-	console.log(event);
-	console.log(msg);
-
-	return process.versions.node;
-});
